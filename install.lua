b8d0bbaee8224e294907902b54ac477ffc8baf2d
Larry Rowe <26939860+larryr1@users.noreply.github.com>
2024-10-09 13:51:02 -0400

    Update install.lua

--- install.lua
+++ install.lua
@@ -5,8 +5,9 @@ local function writeLine(txt)
 end
 
 shell.run("wget run https://raw.githubusercontent.com/michielp1807/more-fonts/refs/heads/main/installer.lua")
-shell.run("wget https://gist.githubusercontent.com/larryr1/4bb2b33d64ed5b22bdc38c85c0422c17/raw/cf7df64bef9698d295840f5ee042d93d8983dd7b/BaseSign.lua")
-shell.run("mv BaseSign.lua startup.lua")
+shell.run("wget https://raw.githubusercontent.com/larryr1/BaseSign/refs/heads/main/application/entry.lua BaseSignEntry.lua")
+shell.run("mkdir basesign_application")
+shell.run("mv BaseSignEntry.lua /basesign_application/entry.lua")
 
 term.clear()
 term.setCursorPos(1, 1)
